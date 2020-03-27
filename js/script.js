f96ae4af6305adbc4a4b6f555fb678785e1351f9
Thomas Mathiot <mathiotthomas@gmail.com>
2020-03-27 10:56:15 +0100

    calculatrice

---
+++ js/script.js
@@ -0,0 +1,81 @@
+var chiffreUn = 0;
+var chiffreDeux = 0;
+var resultatCalc = 0;
+
+function ecrire(nombre){
+	var valText = document.getElementById('textbox').value;
+	document.getElementById('textbox').value = valText + nombre;
+}
+
+function supprimerun(){
+	var valText = document.getElementById('textbox').value;
+	var valTextNew = valText.slice(0, -1);
+	document.getElementById('textbox').value = valTextNew;
+}
+
+function supprimertout(){
+	document.getElementById('textbox').value = '';
+	document.getElementById('operation').value = '';
+
+}
+
+function resultat(){
+	chiffreDeux = parseFloat(document.getElementById('textbox').value);
+	switch (document.getElementById('operation').value){
+		case "+":
+			resultatCalc = chiffreUn + chiffreDeux;
+			break;
+		case "-":
+			resultatCalc = chiffreUn - chiffreDeux;
+			break;
+		case "/":
+			resultatCalc = chiffreUn / chiffreDeux;
+			break;
+		case "X":
+			resultatCalc = chiffreUn * chiffreDeux;
+			break;
+	}
+	document.getElementById('textbox').value = resultatCalc;
+	document.getElementById('operation').value = '';
+}
+
+function operations(ope){
+	chiffreUn = parseFloat(document.getElementById('textbox').value);
+	document.getElementById('textbox').value = '';
+	document.getElementById('operation').value = ope;
+}
+
+
+
+//function resultat(){
+//	var resultat = eval(document.getElementById('textbox').value)
+//	if(resultat){
+//		document.getElementById('textbox').value = resultat;
+//	}
+//}
+
+
+//
+//function addition(){
+//	chiffreUn = parseFloat(document.getElementById('textbox').value);
+//	document.getElementById('textbox').value = '';
+//	document.getElementById('operation').value = "+";
+//}
+//
+//function soustraction(){
+//	chiffreUn = parseFloat(document.getElementById('textbox').value);
+//	document.getElementById('textbox').value = '';
+//	document.getElementById('operation').value = "-";
+//}
+//
+//function division(){
+//	chiffreUn = parseFloat(document.getElementById('textbox').value);
+//	document.getElementById('textbox').value = '';
+//	document.getElementById('operation').value = "/";
+//}
+//
+//function multiplication(){
+//	chiffreUn = parseFloat(document.getElementById('textbox').value);
+//	document.getElementById('textbox').value = '';
+//	document.getElementById('operation').value = "X";
+//}
